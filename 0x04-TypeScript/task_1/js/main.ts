fe7f22e8db112728b9f39777e0796226eff46160
johnson <johnsonawa29@gmail.com>
2025-09-16 22:39:52 +0100

    yes

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -24,3 +24,17 @@ const director1: Directors = {
 };
 
 console.log(director1);
+
+
+// Define the function type
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+// Implement the function
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+// Example usage
+console.log(printTeacher("John", "Doe")); // J. Doe
